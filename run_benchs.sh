714508af9df298d1c35403e5b630a24a12b3c16c
jpinot <josep.pinot@bsc.es>
2025-03-28 12:52:24 +0100

    [run_benchs] Move exectuable files names inside variable

--- run_benchs.sh
+++ run_benchs.sh
@@ -9,6 +9,7 @@ HEAT_BLOCKS=(1024 1296 1600 1936)
 DOT_VECTORS=(4096 8192 16384 32768)
 AXPY_VECTORS=(4096 8192 16384 32768)
 NBLOCKS=(1 2 3 4 5 6)
+executables=(vanilla for record serial)
 
 export LD_LIBRARY_PATH=/usr/clang-x86/lib/x86_64-unknown-linux-gnu/
 
@@ -21,7 +22,8 @@ run_with_threads() {
 }
 
 echo "---> nbody"
-for exe in ./nbody/nbody_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./nbody/nbody_$exe_type"
   for input_file in ./nbody/data/input/nbody_input.in_*; do
     particle_count=$(basename "$input_file" | cut -d'_' -f3)
     run_with_threads "$exe $input_file $particle_count $ITERATIONS"
@@ -29,35 +31,40 @@ for exe in ./nbody/nbody_{vanilla,for,record,serial}; do
 done
 
 echo "--> cholesky"
-for exe in ./cholesky/cholesky_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./cholesky/cholesky_$exe_type"
   for b in "${CHOLESKY_BLOCKS[@]}"; do
     run_with_threads "$exe $b $ITERATIONS"
   done
 done
 
 echo "--> heat"
-for exe in ./heat/heat_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./heat/heat_$exe_type"
   for b in "${HEAT_BLOCKS[@]}"; do
     run_with_threads "$exe ./heat/test.dat $b $ITERATIONS"
   done
 done
 
 echo "--> dot"
-for exe in ./dotp/dot_product_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./dotp/dot_product_$exe_type"
   for c in "${DOT_VECTORS[@]}"; do
     run_with_threads "$exe 131072 $c $ITERATIONS"
   done
 done
 
 echo "--> hog"
-for exe in ./hog/personDetector_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./hog/personDetector_$exe_type"
   for n in "${NBLOCKS[@]}"; do
     run_with_threads "$exe ./hog/input_images/FullHD/otherImages/ 10 $ITERATIONS $n"
   done
 done
 
 echo "--> axpy"
-for exe in ./axpy/axpy_{vanilla,for,record,serial}; do
+for exe_type in "${executables[@]}"; do
+  exe="./axpy/axpy_$exe_type"
   for v in "${AXPY_VECTORS[@]}"; do
     run_with_threads "$exe $v $v $ITERATIONS"
   done
