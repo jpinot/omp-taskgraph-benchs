5bb3d1b784a8e2c826e78a7dc0554504019b3f2a
jpinot <josep.pinot@bsc.es>
2025-07-17 10:28:28 +0200

    [run_benchs] Fix run file

--- run_benchs.sh
+++ run_benchs.sh
@@ -4,11 +4,11 @@ ITERATIONS=64
 THREADS=(8 16 24 32 48)
 
 NBODY_PARTICLES=(4096 2048 1024 512)
-CHOLESKY_BLOCKS=(400 576 784 1024)
-HEAT_BLOCKS=(1024 1296 1600 1936)
-DOT_VECTORS=(4096 8192 16384 32768)
-AXPY_VECTORS=(4096 8192 16384 32768)
-NBLOCKS=(1 2 3 4 5 6)
+CHOLESKY_BLOCKS=(20 24 28 32)
+HEAT_BLOCKS=(32 36 40 44)
+AXPY_VECTORS=(4096 8192 16384 32768 65536)
+DOT_VECTORS=(4 8 16 32 64)
+NBLOCKS=(1 2 3 4)
 executables=(vanilla for record serial)
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/clang-x86/lib/x86_64-unknown-linux-gnu/
@@ -16,10 +16,26 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/clang-x86/lib/x86_64-unknown-linux-
 run_with_threads() {
   local cmd=$1
   for t in "${THREADS[@]}"; do
-    echo "Running: OMP_NUM_THREADS=$t $cmd"
-    OMP_NUM_THREADS=$t $cmd
+    echo "Running: OMP_PROC_BIND='close' OMP_NUM_THREADS=$t $cmd"
+    OMP_PROC_BIND='close' OMP_NUM_THREADS=$t bash -c "$cmd"
   done
 }
+
+echo "--> axpy"
+for exe_type in "${executables[@]}"; do
+  exe="./axpy/axpy_$exe_type"
+  for v in "${AXPY_VECTORS[@]}"; do
+    run_with_threads "$exe 134217728 $v $ITERATIONS"
+  done
+done
+
+echo "--> dot"
+for exe_type in "${executables[@]}"; do
+  exe="./dotp/dot_product_$exe_type"
+  for c in "${DOT_VECTORS[@]}"; do
+    run_with_threads "$exe 131072 $c $ITERATIONS"
+  done
+done
 
 echo "---> nbody"
 for exe_type in "${executables[@]}"; do
@@ -27,6 +43,14 @@ for exe_type in "${executables[@]}"; do
   for input_file in ./nbody/data/input/nbody_input.in_*; do
     particle_count=$(basename "$input_file" | cut -d'_' -f3)
     run_with_threads "$exe $input_file $particle_count $ITERATIONS"
+  done
+done
+
+echo "--> hog"
+for exe_type in "${executables[@]}"; do
+  exe="./hog/personDetector_$exe_type"
+  for n in "${NBLOCKS[@]}"; do
+    run_with_threads "$exe ./hog/input_images/FullHD/otherImages/ 1 $ITERATIONS $n"
   done
 done
 
@@ -46,32 +70,8 @@ for exe_type in "${executables[@]}"; do
   done
 done
 
-echo "--> dot"
-for exe_type in "${executables[@]}"; do
-  exe="./dotp/dot_product_$exe_type"
-  for c in "${DOT_VECTORS[@]}"; do
-    run_with_threads "$exe 131072 $c $ITERATIONS"
-  done
-done
-
-echo "--> hog"
-for exe_type in "${executables[@]}"; do
-  exe="./hog/personDetector_$exe_type"
-  for n in "${NBLOCKS[@]}"; do
-    run_with_threads "$exe ./hog/input_images/FullHD/otherImages/ 10 $ITERATIONS $n"
-  done
-done
-
-echo "--> axpy"
-for exe_type in "${executables[@]}"; do
-  exe="./axpy/axpy_$exe_type"
-  for v in "${AXPY_VECTORS[@]}"; do
-    run_with_threads "$exe $v $v $ITERATIONS"
-  done
-done
-
 echo "--> nas-omp"
-for exe in ./nas-omp/bin/*; do
+for exe in ./nas-omp/bin/cg_taskloop*; do
     run_with_threads "$exe"
 done
 
